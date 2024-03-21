beffcdee7442892ed328415604fa4c0fbbcb5ef3
eng-shahd <158595641+eng-shahd@users.noreply.github.com>
2024-03-21 16:29:51 +0200

    Update CSV_RepresentationOfArray.js

--- js_tasks/CSV_RepresentationOfArray.js
+++ js_tasks/CSV_RepresentationOfArray.js
@@ -1,19 +1,26 @@
-// enter the array
-const Array = [
-    [ 0, 1, 2, 3, 4 ],
-    [ 10, 11, 12, 13, 14 ],
-    [ 20, 21, 22, 23, 24 ],
-    [ 30, 31, 32, 33, 34 ]
-];
-// the  function
-function CSV(array) {
+function toCsvText(array) {
     let csv = '';
     for (let i = 0; i < array.length; i++) {
-        csv += arr[i].join(',') + '\n';
+        csv += array[i].join(',') + '\n';
     }
     csv = csv.slice(0, -1);
     return csv;
 }
 
-// print the output
-console.log(CSV(Array));
+//test
+console.log(toCsvText([[ 0, 1, 2, 3, 45 ],
+                      [ 10,11,12,13,14 ],
+                      [ 20,21,22,23,24 ],
+                      [ 30,31,32,33,34 ]
+                      ] ));
+console.log("============================================\n")
+console.log(toCsvText([
+                       [ -25, 21, 2, -33, 48 ],
+                       [ 30,31,-32,33,-34 ]
+                        ] ));
+console.log("============================================\n")
+console.log(toCsvText([
+                       [ 5,55,5,5,55 ],
+                       [ 6,6,66,23,24 ],
+                       [ 666,31,66,33,7 ]
+                       ] ));
